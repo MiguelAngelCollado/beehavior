eda7b3af1f5275257517d54f0d294f4411b3c1ad
MiguelAngelCollado <xmiguelangelcollado@gmail.com>
2018-06-15 15:20:47 +0200

    up

--- scripts/Data construction.R
+++ scripts/Data construction.R
@@ -2,4 +2,6 @@ Beeh.data<-read.csv("~/Desktop/Tesis/R/beehavior/data/Behavior comparison.csv")
 View(Beeh.data)
 
 summary(Beeh.data$Species)
+summary(Beeh.data$Place)
+
 which(summary(Beeh.data$Species)>2)
