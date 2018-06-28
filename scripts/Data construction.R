723983ce6b45774efc6db150838e6a8f9313582e
MiguelAngelCollado <xmiguelangelcollado@gmail.com>
2018-06-29 00:27:23 +0200

    Starting!

--- scripts/Data construction.R
+++ scripts/Data construction.R
@@ -1,31 +1,65 @@
+library(dplyr)
 Beeh.data<-read.csv("~/Desktop/Tesis/R/beehavior/data/Behavior comparison.csv")
 View(Beeh.data)
 
-#Data treatment-----
-#PER variables must be numerical
-str(Beeh.data)
-Beeh.data$PER.sugar1<-as.numeric(Beeh.data$PER.sugar1)
-Beeh.data$PER.sugar2<-as.numeric(Beeh.data$PER.sugar2)
-Beeh.data$PER.sugar3<-as.numeric(Beeh.data$PER.sugar3)
-Beeh.data$PER.sugar4<-as.numeric(Beeh.data$PER.sugar4)
-Beeh.data$PER.sugar5<-as.numeric(Beeh.data$PER.sugar5)
-Beeh.data$PER.sugar6<-as.numeric(Beeh.data$PER.sugar6)
-Beeh.data$PER.sugar7<-as.numeric(Beeh.data$PER.sugar7)
-Beeh.data$PER.sugar.test<-as.numeric(Beeh.data$PER.sugar.test)
-Beeh.data$PER.water1<-as.numeric(Beeh.data$PER.water1)
-Beeh.data$PER.water2<-as.numeric(Beeh.data$PER.water2)
-Beeh.data$PER.water3<-as.numeric(Beeh.data$PER.water3)
-Beeh.data$PER.water4<-as.numeric(Beeh.data$PER.water4)
-Beeh.data$PER.water5<-as.numeric(Beeh.data$PER.water5)
-Beeh.data$PER.water6<-as.numeric(Beeh.data$PER.water6)
-Beeh.data$PER.water.test<-as.numeric(Beeh.data$PER.water.test)
+#Useless bees and useful bees----
 
-#####
+######
+useless<-which(is.na(Beeh.PER.sugar$PER.sugar1) & 
+  is.na(Beeh.PER.sugar$PER.sugar2) & 
+  is.na(Beeh.PER.sugar$PER.sugar3) & 
+  is.na(Beeh.PER.sugar$PER.sugar4) & 
+  is.na(Beeh.PER.sugar$PER.sugar5) & 
+  is.na(Beeh.PER.sugar$PER.sugar6) & 
+  is.na(Beeh.PER.sugar$PER.sugar7) & 
+  is.na(Beeh.PER.sugar$PER.sugar.test))
+useless.bees<-Beeh.data[useless,]
 
+functional.bees<-Beeh.data[-useless,]
+
+######
+
+
+#Initial exploration----
+summary(Beeh.data$Genus)
+summary(Beeh.data$Species)
 which(summary(Beeh.data$Species)>2)
 
+#Data treatment and construction-----
+#PER variables must be numerical (pasalos a as.character primero)
+str(Beeh.data)
+Beeh.data$PER.sugar1<-as.numeric(as.character(Beeh.data$PER.sugar1))
+Beeh.data$PER.sugar2<-as.numeric(as.character(Beeh.data$PER.sugar2))
+Beeh.data$PER.sugar3<-as.numeric(as.character(Beeh.data$PER.sugar3))
+Beeh.data$PER.sugar4<-as.numeric(as.character(Beeh.data$PER.sugar4))
+Beeh.data$PER.sugar5<-as.numeric(as.character(Beeh.data$PER.sugar5))
+Beeh.data$PER.sugar6<-as.numeric(as.character(Beeh.data$PER.sugar6))
+Beeh.data$PER.sugar7<-as.numeric(as.character(Beeh.data$PER.sugar7))
+Beeh.data$PER.sugar.test<-as.numeric(as.character(Beeh.data$PER.sugar.test))
+Beeh.data$PER.water1<-as.numeric(as.character(Beeh.data$PER.water1))
+Beeh.data$PER.water2<-as.numeric(as.character(Beeh.data$PER.water2))
+Beeh.data$PER.water3<-as.numeric(as.character(Beeh.data$PER.water3))
+Beeh.data$PER.water4<-as.numeric(as.character(Beeh.data$PER.water4))
+Beeh.data$PER.water5<-as.numeric(as.character(Beeh.data$PER.water5))
+Beeh.data$PER.water6<-as.numeric(as.character(Beeh.data$PER.water6))
+Beeh.data$PER.water.test<-as.numeric(as.character(Beeh.data$PER.water.test))
+
+
 Beeh.data
-Beeh.PER.sugar<-data.frame(Beeh.data$ID,Beeh.data$Species,Beeh.data$PER.sugar1,Beeh.data$PER.sugar2,Beeh.data$PER.sugar3,Beeh.data$PER.sugar4,Beeh.data$PER.sugar5,Beeh.data$PER.sugar6,Beeh.data$PER.sugar7,Beeh.data$PER.sugar.test)
+nrow(Beeh.data)
+nrow(Beeh.PER.sugar)
+#PER.sugar data.frame
+Beeh.PER.sugar<-data.frame(Beeh.data$ID,
+                           Beeh.data$Species,
+                           Beeh.data$PER.sugar1,
+                           Beeh.data$PER.sugar2,
+                           Beeh.data$PER.sugar3,
+                           Beeh.data$PER.sugar4,
+                           Beeh.data$PER.sugar5,
+                           Beeh.data$PER.sugar6,
+                           Beeh.data$PER.sugar7,
+                           Beeh.data$PER.sugar.test)
+
 Beeh.PER.sugar<-rename(Beeh.PER.sugar, 
        ID = Beeh.data.ID, 
        Species = Beeh.data.Species,
@@ -41,32 +75,53 @@ Beeh.PER.sugar<-rename(Beeh.PER.sugar,
 plot(t(Beeh.PER.sugar[16,(3:10)]), xlab="Trial number", ylab = "Time")
 lines(t(Beeh.PER.sugar[16,(3:10)]))
 
+View(Beeh.PER.sugar)
+is.na(Beeh.PER.sugar)
 par(mfrow = c(3,3))
+n=1
+
+#Number of individuals that reacted to the test#
+
+reactors<-NULL
 for (n in 1:(nrow(Beeh.PER.sugar))) {
-  print(plot(t(Beeh.PER.sugar[n,(3:10)]), xlab="Trial number", ylab = "Time"))
+  if(is.na(Beeh.PER.sugar$PER.sugar1[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar2[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar3[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar4[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar5[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar6[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar7[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar.test[n])==TRUE){
+    reactors[n]<-NA}else{                                                                                                                                                                                                                       
+    reactors[n]<-Beeh.PER.sugar$Species[n]  
+    }}
+reactors
+#The number is
+length(which(is.na(reactors)==FALSE))
+
+#Graphs of every individual for PER sugar
+colnames(Beeh.PER.sugar)
+par(mfrow = c(3,4))
+for (n in 1:(nrow(Beeh.PER.sugar))) {
+  if(is.na(Beeh.PER.sugar$PER.sugar1[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar2[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar3[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar4[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar5[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar6[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar7[n]) & 
+     is.na(Beeh.PER.sugar$PER.sugar.test[n])){
+    print("This bee does not work")}else{                                                                                                                                                                                                                       
+  print(plot(t(Beeh.PER.sugar[n,(3:10)]), xlab="Trial number", ylab = "Time", main = (Beeh.PER.sugar$Species[n])))
   lines(t(Beeh.PER.sugar[n,(3:10)]))
-  
-}
-plot(t(Beeh.PER.sugar[1,(3:10)]), xlab="Trial number", ylab = "Time")
+}}
+par(mfrow = c(1,1))
+
+
+#Individual graphs
+par(mfrow = c(1,1))
+plot(t(Beeh.PER.sugar[1,(3:10)]), xlab="Trial number", ylab = "Time", main=(Beeh.PER.sugar$Beeh.data.Species))
 lines(t(Beeh.PER.sugar[1,(3:10)]))
-plot(t(Beeh.PER.sugar[2,(3:10)]), xlab="Trial number", ylab = "Time")
-lines(t(Beeh.PER.sugar[2,(3:10)]))
 
 
 
-print(plot(t(activity.decline[n,]), ylim = c(0,1), xlab = "Trial", ylab = "activity.prop", main = row.names(activity.decline[n,])))
-lines(t(activity.decline[n,]), ylim = c(0,1))
-
-plot()
-points(Beeh.data[16,14],Beeh.data[16,18],Beeh.data[16,22])
-colnames(Beeh.data)
-
-
-innovation.curve<- survfit(Surv(virtual.success.time5, success5) ~ experiment.type, na.action = na.exclude, data = explain.innovation1.full) 
-plot(innovation.curve, lty = 1:2, xlab="Virtual success time 5", ylab="% of no success in trial 5", main= "Kapdddlan-Meier Curves\nfor innovation") 
-legend(10000, .4, c("Control","Treatment"), lty = 1:2) 
-
-#There is no difference in Treatment vs Control curves
-survdiff (Surv(virtual.success.time5, success5) ~ experiment.type, na.action = na.exclude, data = explain.innovation1.full)
-
-points(Beeh.data[1,1])
