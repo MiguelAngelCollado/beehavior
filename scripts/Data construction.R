ddb09293587c1116d89d00fc971c4ef0132c63a1
MiguelAngelCollado <xmiguelangelcollado@gmail.com>
2018-07-04 18:34:59 +0200

    Doing some figurines

--- scripts/Data construction.R
+++ scripts/Data construction.R
@@ -2,7 +2,17 @@ library(dplyr)
 Beeh.data<-read.csv("~/Desktop/Tesis/R/beehavior/data/Behavior comparison.csv")
 
 #How many individuals do we have?----
+nrow(Beeh.data)
 summary(Beeh.data$Genus)
+summary(Beeh.data$Species)
+n.of.species<-as.data.frame(summary(Beeh.data$Species))
+colnames(n.of.species)<-"individuals captured"
+#We export the list for the article
+getwd()
+setwd("/Users/Bartomeus_lab/Desktop/Tesis/R/beehavior/figures")
+write.csv(n.of.species, "list of species.csv")
+setwd("/Users/Bartomeus_lab/Desktop/Tesis/R/beehavior")
+
 nrow(Beeh.data)
 length(summary(Beeh.data$Species))
 
@@ -79,7 +89,7 @@ functional.bees
 #Data frame of bees that did not work
 useless.bees
 
-#Let's identify which species did not work
+#Let's identify which species did not work at all
 useless.bees
 useless.species.temp<-unique(useless.bees$Species)
 useless.species.temp
@@ -116,13 +126,10 @@ useless.species<-rbind(useless.species,temp)
 useless.species<-droplevels.data.frame(useless.species)
 useless.species
 summary(useless.species$Species)
-
-
-
-
-
-
-
+getwd()
+setwd("/Users/Bartomeus_lab/Desktop/Tesis/R/beehavior/figures")
+write.csv(summary(useless.species$Species), "list of useless species.csv")
+setwd("/Users/Bartomeus_lab/Desktop/Tesis/R/beehavior")
 
 
 
@@ -161,4 +168,30 @@ plot(t(Beeh.PER.sugar[1,(3:10)]), xlab="Trial number", ylab = "Time", main=(Beeh
 lines(t(Beeh.PER.sugar[1,(3:10)]))
 
 
+#Some bees, spent too much time between tasting one cue and the other, let's explore
+#that
 
+max.xlim<-max(na.omit(c(Beeh.data$PER.sugar1,Beeh.data$PER.sugar2,Beeh.data$PER.sugar3,Beeh.data$PER.sugar4,Beeh.data$PER.sugar5,Beeh.data$PER.sugar6,Beeh.data$PER.sugar7,Beeh.data$PER.sugar.test,Beeh.data$PER.water1,Beeh.data$PER.water2,Beeh.data$PER.water3,Beeh.data$PER.water4,Beeh.data$PER.water5,Beeh.data$PER.water6,Beeh.data$PER.water7,Beeh.data$PER.water.test)))
+par(mfrow=c(2,2))
+plot(abs(Beeh.data$PER.sugar1 - Beeh.data$PER.water1), row.names(Beeh.data), xlab="Time difference between sugar PER and water PER", ylab="Individual", main = "Trial 1", ylim = c(0,nrow(Beeh.data)), xlim = c(0,max.xlim))
+abline(v = 120)
+plot(abs(Beeh.data$PER.sugar2 - Beeh.data$PER.water2), row.names(Beeh.data), xlab="Time difference between sugar PER and water PER", ylab="Individual", main = "Trial 2", ylim = c(0,nrow(Beeh.data)), xlim = c(0,max.xlim))
+abline(v = 120)
+plot(abs(Beeh.data$PER.sugar3 - Beeh.data$PER.water3), row.names(Beeh.data), xlab="Time difference between sugar PER and water PER", ylab="Individual", main = "Trial 3", ylim = c(0,nrow(Beeh.data)), xlim = c(0,max.xlim))
+abline(v = 120)
+plot(abs(Beeh.data$PER.sugar4 - Beeh.data$PER.water4), row.names(Beeh.data), xlab="Time difference between sugar PER and water PER", ylab="Individual", main = "Trial 4", ylim = c(0,nrow(Beeh.data)), xlim = c(0,max.xlim))
+abline(v = 120)
+plot(abs(Beeh.data$PER.sugar5 - Beeh.data$PER.water5), row.names(Beeh.data), xlab="Time difference between sugar PER and water PER", ylab="Individual", main = "Trial 5", ylim = c(0,nrow(Beeh.data)), xlim = c(0,max.xlim))
+abline(v = 120)
+plot(abs(Beeh.data$PER.sugar6 - Beeh.data$PER.water6), row.names(Beeh.data), xlab="Time difference between sugar PER and water PER", ylab="Individual", main = "Trial 6", ylim = c(0,nrow(Beeh.data)), xlim = c(0,max.xlim))
+abline(v = 120)
+plot(abs(Beeh.data$PER.sugar7 - Beeh.data$PER.water7), row.names(Beeh.data), xlab="Time difference between sugar PER and water PER", ylab="Individual", main = "Trial 7", ylim = c(0,nrow(Beeh.data)), xlim = c(0,max.xlim))
+abline(v = 120)
+plot(abs(Beeh.data$PER.sugar.test - Beeh.data$PER.water.test), row.names(Beeh.data), ylab="Individual", xlab="Time difference between sugar PER and water PER", main = "Test", ylim = c(0,nrow(Beeh.data)), xlim = c(0,max.xlim))
+abline(v = 120)
+par(mfrow=c(1,1))
+
+par(mfrow=c(1,2))
+
+boxplot(Beeh.data$PER.sugar1,Beeh.data$PER.sugar2,Beeh.data$PER.sugar3,Beeh.data$PER.sugar4,Beeh.data$PER.sugar5,Beeh.data$PER.sugar6,Beeh.data$PER.sugar7,Beeh.data$PER.sugar.test, main= "PER sugar dispersion along trials", names = c("Trial 1","Trial 2","Trial 3","Trial 4","Trial 5","Trial 6","Trial 7","Test"), ylab="Time", ylim=c(0,max.xlim))
+boxplot(Beeh.data$PER.water1 ,Beeh.data$PER.water2,Beeh.data$PER.water3,Beeh.data$PER.water4,Beeh.data$PER.water5,Beeh.data$PER.water6,Beeh.data$PER.water7,Beeh.data$PER.water.test, main= "PER water dispersion along trials", names = c("Trial 1","Trial 2","Trial 3","Trial 4","Trial 5","Trial 6","Trial 7","Test"), ylab="Time", ylim=c(0,max.xlim))
